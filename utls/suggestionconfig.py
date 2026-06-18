06bf5edd6e20e2d9d346597e9d17d88cf7402278
elliodoming.u.e.z2.3.6.9@gmail.com <219328601+Akshsays@users.noreply.github.com>
2026-06-18 21:10:18 +0530

    major changes: merged suggester_id and suggestion_id into one table

--- utls/suggestionconfig.py
+++ utls/suggestionconfig.py
@@ -10,18 +10,19 @@ def init_table():
     con=get_connected()
     cursor=con.cursor()
 
-    cursor.execute("CREATE TABLE IF NOT EXISTS suggestion_channels(guild_id INT PRIMARY KEY,channel_id INT NOT NULL)")
+    cursor.execute("CREATE TABLE IF NOT EXISTS suggestion_channels(guild_id INT PRIMARY KEY,channel_id INT NOT NULL,suggester_id INT,suggestion_id INT)")
     
     con.commit()
     cursor.close()
 
-def init_member():
+def set_suggestion_channel(guild_id:int,channel_id:int):
 
     con=get_connected()
     cursor=con.cursor()
 
-    cursor.execute("CREATE TABLE IF NOT EXISTS suggestion_info(guild_id INT PRIMARY KEY,suggester_id INT NOT NULL,suggestion_id INT NOT NULL)")
-    
+    query="INSERT OR REPLACE INTO suggestion_channels(guild_id, channel_id) VALUES(?,?)"
+    cursor.execute(query,(guild_id,channel_id))
+
     con.commit()
     cursor.close()
 
@@ -30,38 +31,8 @@ def store_info(guild_id:int,suggester_id:int,suggestion_id:int):
     con=get_connected()
     cursor=con.cursor()
 
-    query="INSERT OR REPLACE INTO suggestion_info(guild_id,suggester_id,suggestion_id) VALUES(?,?,?)"
-    cursor.execute(query,(guild_id,suggester_id,suggestion_id))
-
-    con.commit()
-    cursor.close()
-
-def get_info(guild_id:int):
-
-    con=get_connected()
-    cursor=con.cursor()
-
-    query="SELECT suggester_id from suggestion_info WHERE guild_id=?"
-    cursor.execute(query,(int(guild_id),))
-
-    result=cursor.fetchone()
-    value=int(result[0])
-    cursor.close()
-
-    if value:
-        suggester_id=value
-
-        return int(suggester_id)
-    else:
-        return None
-
-def set_suggestion_channel(guild_id:int,channel_id:int):
-
-    con=get_connected()
-    cursor=con.cursor()
-
-    query="INSERT OR REPLACE INTO suggestion_channels VALUES(?,?)"
-    cursor.execute(query,(guild_id,channel_id))
+    query="UPDATE suggestion_channels SET suggester_id=?, suggestion_id=? where guild_id=?"
+    cursor.execute(query,(suggester_id,suggestion_id,guild_id))
 
     con.commit()
     cursor.close()
@@ -83,5 +54,24 @@ def get_suggestion_channel(guild_id:int):
         channel_id=value
 
         return int(channel_id)
+    else:
+        return None
+
+def get_info(guild_id:int):
+
+    con=get_connected()
+    cursor=con.cursor()
+
+    query="SELECT suggester_id from suggestion_channels WHERE guild_id=?"
+    cursor.execute(query,(int(guild_id),))
+
+    result=cursor.fetchone()
+    value=int(result[0])
+    cursor.close()
+
+    if value:
+        suggester_id=value
+
+        return int(suggester_id)
     else:
         return None
